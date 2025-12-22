dc78830f87c394cf9af7384bb75d30acdd101e84
claxn <clax.n@gmx.at>
2025-12-22 23:00:43 +0100

    changed visibility of error module and tiling access

--- src/pytileproj/__init__.py
+++ src/pytileproj/__init__.py
@@ -1,11 +1,7 @@
 """pytileproj's init module defining outward facing objects."""
 
-# from pytileproj.grid import RegularGrid # noqa: ERA001
-# from pytileproj.tile import RasterTile # noqa: ERA001
-# from pytileproj.tiling_system import ProjCoord, RegularProjTilingSystem # noqa: ERA001
+from pytileproj.grid import RegularGrid
+from pytileproj.tile import RasterTile
+from pytileproj.tiling_system import ProjCoord, RegularProjTilingSystem
 
-
-#    "ProjCoord", # noqa: ERA001
-#    "RasterTile", # noqa: ERA001
-#    "RegularGrid", # noqa: ERA001
-#    "RegularProjTilingSystem", # noqa: ERA001
+__all__ = ["ProjCoord", "RasterTile", "RegularGrid", "RegularProjTilingSystem"]
