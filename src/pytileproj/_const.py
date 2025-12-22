44192fa1702832e8201dd7a65b7ad7dcd5c8a891
claxn <clax.n@gmx.at>
2025-12-22 23:08:56 +0100

    changed importutil

--- src/pytileproj/_const.py
+++ src/pytileproj/_const.py
@@ -1,14 +1,10 @@
-import importlib.util
+from importlib.util import find_spec
 
 DEFAULT_SEG_NUM = 25000  # number of segments for a geometry
 DECIMALS = 9  # least significant digit after the command for rounding
 TIMEOUT = 60  # timeout for requests
 JSON_INDENT = 4  # indentation for JSON strings
 
-VIS_INSTALLED = None not in [
-    importlib.util.find_spec(pkg) for pkg in ["matplotlib", "cartopy"]
-]
+VIS_INSTALLED = None not in [find_spec(pkg) for pkg in ["matplotlib", "cartopy"]]
 
-GEO_INSTALLED = None not in [
-    importlib.util.find_spec(pkg) for pkg in ["geopandas", "pyarrow"]
-]
+GEO_INSTALLED = None not in [find_spec(pkg) for pkg in ["geopandas", "pyarrow"]]
