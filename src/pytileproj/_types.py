311a94c7fbcc73932f68f2360dfdb80eac522aaf
Nikolas Pikall <nikolas.pikall@gmail.com>
2025-12-31 13:19:22 +0100

    refactor: add TypeHints
    - add TypeAlias TypeHints
    - removed 'int' as it is implied by 'float'

--- src/pytileproj/_types.py
+++ src/pytileproj/_types.py
@@ -1,5 +1,5 @@
 from collections.abc import Generator
-from typing import Any, TypeVar
+from typing import Any, TypeAlias, TypeVar
 
 from morecantile.models import Tile as RegularTile
 
@@ -7,10 +7,10 @@ from pytileproj.tile import IrregularTile, RasterTile
 
 T_co = TypeVar("T_co", covariant=True)
 RT = TypeVar("RT", bound="RasterTile[Any]")
-Extent = tuple[int | float, int | float, int | float, int | float]
-PartialExtent = tuple[int | float, int | float, int | float | None, int | float | None]
-AnyTile = RegularTile | IrregularTile
-TileGenerator = Generator[AnyTile, AnyTile, AnyTile]
-RegTileGenerator = Generator[RegularTile, RegularTile, RegularTile]
-IrregTileGenerator = Generator[IrregularTile, IrregularTile, IrregularTile]
-RasterTileGenerator = Generator[RasterTile[T_co], RasterTile[T_co], RasterTile[T_co]]
+Extent: TypeAlias = tuple[float, float, float, float]
+PartialExtent: TypeAlias = tuple[float, float, float | None, float | None]
+AnyTile: TypeAlias = RegularTile | IrregularTile
+TileGenerator: TypeAlias = Generator[AnyTile, None, None]
+RegTileGenerator: TypeAlias = Generator[RegularTile, None, None]
+IrregTileGenerator: TypeAlias = Generator[IrregularTile, None, None]
+RasterTileGenerator: TypeAlias = Generator[RasterTile[T_co], None, None]
